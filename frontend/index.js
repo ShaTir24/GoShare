e242f688117351442445054e53fc810f06a6ec4f
Tirth Shah <tirthshah100@gmail.com>
2023-07-27 21:37:14 +0530

    updated server url

--- frontend/index.js
+++ frontend/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://go-share.vercel.app";
+const baseURL = "https://go-share-apis.onrender.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
